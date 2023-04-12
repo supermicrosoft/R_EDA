b86c4eb21b7f756760ce228e6c57639a5e3ee03b
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2023-04-12 13:09:46 +0900

    2023-04-12 6주 자료의 재표현

--- (과제)5주_상자그림.R
+++ (과제)5주_상자그림.R
@@ -1,12 +1,37 @@
 # 박스그림 그리기 : boxplot()
 # reorder(지역명, 지역코드)
+
 head(iris)
 attach(iris)
 par(mfrow=c(2,2))
 boxplot(Sepal.Length~Species, col=c("Deep Sky Blue 2", "Sky Blue 2", "Light Sky Blue 2"))
 
 boxplot(Sepal.Width~Species, col=c("Deep Sky Blue 2", "Sky Blue 2", "Light Sky Blue 2"))
+mean(setosa$Petal.Length)
+summary(setosa$Petal.Width)
 
 boxplot(Petal.Length~Species, col=c("Deep Sky Blue 2", "Sky Blue 2", "Light Sky Blue 2"))
 
 boxplot(Petal.Width~Species, col=c("Deep Sky Blue 2", "Sky Blue 2", "Light Sky Blue 2"))
+mean(setosa$Petal.Width)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+iris % > % 
+  select(Sepal.Width) % > % 
+  plot(mapping = aes(x = Sepal.Width))+
+  stat_density(alpha = 0.7,
+               fill = 'steelblue',
+               color = 'blue')+
+  labs(title = 'Distribution:Sepal.Width')
